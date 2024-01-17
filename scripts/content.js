4a2eb2fb05cc497a7953f75d05676fa6f7f54e65
Renato Cipolla <renato.cipolla94@gmail.com>
2024-01-17 21:07:43 +0000

    add updateWordCountBadge

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,24 @@
-const body = document.body;
-
-if (body) {
-  const text = body.textContent;
-  console.log(text)
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  const wordCount = [...words].length;
-  const badge = document.createElement("p");
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `This page contains ${wordCount} words`;
-  document.body.prepend(badge);
-}
+const updateWordCountBadge = () => {
+    const text = document.body.innerText;
+    const wordMatchRegExp = /\b\w+\b/g;
+    const words = text.matchAll(wordMatchRegExp);
+    const wordCount = [...words].length;
+  
+    const badge = document.querySelector("#wordCountBadge");
+  
+    if (badge) {
+      badge.textContent = `This page contains ${wordCount} words`;
+    } else {
+      const newBadge = document.createElement("p");
+      newBadge.id = "wordCountBadge";
+      newBadge.classList.add("color-secondary-text", "type--caption");
+      newBadge.textContent = `This page contains ${wordCount} words`;
+      document.body.prepend(newBadge);
+    }
+  };
+  
+  updateWordCountBadge();
+  const observer = new MutationObserver(updateWordCountBadge);
+  const config = { childList: true, subtree: true };
+  observer.observe(document.body, config);
+  
