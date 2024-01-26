e67876fc37f2c79294d6ac40250e0e4089502020
Renato Cipolla <renato.cipolla94@gmail.com>
2024-01-26 11:14:09 +0000

    add count for any word

--- popup.js
+++ popup.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function countWords() {
-  const wordsInput = document.getElementById("wordsInput").value;
+  const wordInput = document.getElementById("wordInput").value;
 
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
       tabs[0].id,
-      { action: "countWords", wordsInput: wordsInput },
+      { action: "countWords", wordInput: wordInput },
       function (response) {
         const resultMessage =
           response && response.wordCount !== undefined
