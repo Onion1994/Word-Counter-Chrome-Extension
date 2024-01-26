9e17b43240cdb6a0ebfcc55e02380491a1e259af
Renato Cipolla <renato.cipolla94@gmail.com>
2024-01-26 10:25:48 +0000

    add form to popup.html

--- popup.js
+++ popup.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("countButton").addEventListener("click", countWords);
+  document.getElementById("countForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    countWords();
+  });
 });
 
 function countWords() {
+  const wordsInput = document.getElementById("wordsInput").value;
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
       tabs[0].id,
-      { action: "countWords" },
+      { action: "countWords", wordsInput: wordsInput },
       function (response) {
         const resultMessage =
           response && response.wordCount !== undefined
