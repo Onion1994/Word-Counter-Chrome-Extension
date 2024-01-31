e38a99cc366fa52aa4a575162c4af9ae1100e6e8
Renato Cipolla <renato.cipolla94@gmail.com>
2024-01-31 14:51:10 +0000

    add small refactoring to content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,19 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  const { wordInput, action } = request;
-  if (action === "countWords") {
-    const text = document.body.innerText || document.body.textContent;
-    if (wordInput.length > 0) {
-      const wordCount = text.split(/\s+/).filter(function (word) {
-        const cleanedWord = word.replace(/[^\w\s]/g, '');
-        return cleanedWord.length > 0 && cleanedWord.toUpperCase() === wordInput.toUpperCase();
-      }).length;      
-      sendResponse({ wordCount: wordCount });
-    } else {
-      const allWordCount = text.split(/\s+/).filter(function (word) {
-        return word.length > 0;
-      }).length;
-      sendResponse({ wordCount: allWordCount });
-    }
+  const { wordInput } = request;
+  const text = document.body.innerText || document.body.textContent;
+  if (wordInput.length > 0) {
+    const wordCount = text.split(/\s+/).filter(function (word) {
+      const cleanedWord = word.replace(/[^\w\s]/g, "");
+      return (
+        cleanedWord.length > 0 &&
+        cleanedWord.toUpperCase() === wordInput.toUpperCase()
+      );
+    }).length;
+    sendResponse({ wordCount: wordCount });
+  } else {
+    const allWordCount = text.split(/\s+/).filter(function (word) {
+      return word.length > 0;
+    }).length;
+    sendResponse({ wordCount: allWordCount });
   }
 });
