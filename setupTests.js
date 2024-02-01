320539f4da34da92986b5ea93fb560a7bbc6e1cd
Renato Cipolla <renato.cipolla94@gmail.com>
2024-02-01 13:59:05 +0000

    add fixed dom to setupTests.js

--- setupTests.js
+++ setupTests.js
@@ -1,9 +1,25 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
 
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
+const dom = new JSDOM(`<!DOCTYPE html>
+<html>
+<head>
+  <title>Word Counter</title>
+  <link rel="stylesheet" type="text/css" href="popup.css">
+</head>
+<body>
+  <form id="countForm">
+    <label for="wordInput">Enter a word or leave empty to count all words</label>
+    <input type="text" id="wordInput" placeholder="Type a word here">
+    <br>
+    <input type="submit" value="Count!">
+  </form>
+  <p id="wordCount"></p>
+  <script src="popup.js"></script>
+</body>
+</html>`);
 global.document = dom.window.document;
 global.window = dom.window;
 
 global.navigator = {
-  userAgent: 'node.js',
+  userAgent: "node.js",
 };
