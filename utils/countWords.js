050cdfbdd1b6a401d6acc1cec74c4a152a55717e
Renato Cipolla <renato.cipolla94@gmail.com>
2024-02-07 19:44:23 +0000

    add refactoring to countWords

--- utils/countWords.js
+++ utils/countWords.js
@@ -1,19 +1,17 @@
 function countWords(text, wordInput) {
-    if (wordInput.trim().length > 0) {
-      return text.split(/\b/).filter(function (word) {
-        const cleanedWord = word.replace(/[^\w\s']/g, ""); 
-        return (
-          cleanedWord.length > 0 &&
-          cleanedWord.toUpperCase() === wordInput.toUpperCase().trim()
-        );
-      }).length;
-    } else {
-      return text.split(/\s+/).filter(function (word) {
-        return word.length > 0;
-      }).length;
-    }
+  const trimmedWordInput = wordInput.trim();
+  if (trimmedWordInput.length > 0) {
+    return text.split(/\b/).filter(function (word) {
+      const cleanedWord = word.replace(/[^\w\s']/g, "");
+      return cleanedWord.toUpperCase() === trimmedWordInput.toUpperCase();
+    }).length;
+  } else {
+    return text.split(/\s+/).filter(function (word) {
+      return word.length > 0;
+    }).length;
   }
-  
-  if (typeof module !== "undefined" && module.exports) {
-    module.exports = { countWords };
-  }
+}
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { countWords };
+}
